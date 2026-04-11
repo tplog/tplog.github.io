438a127fb7f9846ca5266c3bef9014db0dc34806
tplog <suzulang.jiang@gmail.com>
2026-04-12 03:22:12 +0800

    chore: update site title to 人生現役

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'PaperAstro';
+export const SITE_TITLE = '人生現役';
 export const SITE_DESCRIPTION = 'A hand‑drawn, lightweight Astro starter built with PaperCSS.';
 
